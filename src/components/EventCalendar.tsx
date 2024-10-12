f3a1e63b07450a42a1aa4158af58bcf213a99ccb
Piya Miang-Lae <87295280+Piya-Boy@users.noreply.github.com>
2024-10-13 01:42:30 +0700

    Events Component Design

---
+++ src/components/EventCalendar.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { MoreHorizOutlined } from '@mui/icons-material';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+const events = [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor",
+      time: "12:00 PM - 2:00 PM",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor",
+      time: "12:00 PM - 2:00 PM",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum dolor",
+      time: "12:00 PM - 2:00 PM",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+  ];
+
+export default function EventCalendar() {
+    const [value, onChange] = useState<Value>(new Date());
+
+  return (
+    <div className="bg-white dark:bg-slate-700 p-4 rounded-lg">
+      <Calendar
+      className="bg-white dark:bg-slate-700 dark:text-slate-400"
+        value={value}
+        onChange={onChange}
+      />
+       <div className="flex items-center justify-between dark:text-slate-400">
+        <h1 className="text-xl font-semibold my-4">Events</h1>
+        <MoreHorizOutlined />
+      </div>
+      <div className="flex flex-col gap-4">
+        {events.map((event) => (
+          <div
+            className="p-5 rounded-md border-2  border-gray-100 border-t-4 odd:border-t-lamaSky even:border-t-lamaPurple"
+            key={event.id}
+          >
+            <div className="flex items-center justify-between">
+              <h1 className="font-semibold text-gray-600 dark:text-slate-200">{event.title}</h1>
+              <span className="text-gray-400 text-xs">{event.time}</span>
+            </div>
+            <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
