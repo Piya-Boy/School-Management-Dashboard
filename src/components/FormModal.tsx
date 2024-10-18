4fb2858d44dc6b6cf970a89a26aa95b7406cfe67
Piya Miang-Lae <87295280+Piya-Boy@users.noreply.github.com>
2024-10-18 18:44:20 +0700

    Dynamic Form Buttons (Create, Update, Delete)

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,6 +1,55 @@
+"use client";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import { useState } from "react";
+export default function FormModal({
+  table,
+  type,
+  data,
+  id,
+}: {
+  table:
+    | "teacher"
+    | "student"
+    | "parent"
+    | "subject"
+    | "class"
+    | "lesson"
+    | "exam"
+    | "assignment"
+    | "result"
+    | "attendance"
+    | "event"
+    | "announcement";
+  type: "create" | "update" | "delete";
+  data?: any;
+  id?: number;
+}) {
+  const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
+  const bgColor =
+    type === "create"
+      ? "bg-lamaYellow"
+      : type === "update"
+      ? "bg-lamaSky"
+      : "bg-lamaPurple";
 
-export default function FormModal() {
+  const [open, setOpen] = useState(false);
+  const Icon =
+    type === "create"
+      ? AddOutlinedIcon
+      : type === "update"
+      ? EditOutlinedIcon
+      : DeleteOutlineOutlinedIcon;
+
   return (
-    <div>FormModal</div>
-  )
+    <>
+      <button
+        className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
+        onClick={() => setOpen(true)}
+      >
+        <Icon className="text-gray-400 dark:text-gray-500" />
+      </button>
+    </>
+  );
 }
