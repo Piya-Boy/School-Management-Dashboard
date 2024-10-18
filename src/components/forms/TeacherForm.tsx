3cfd1359e6ec8b831913bb8facdd0da8c0315322
Piya Miang-Lae <87295280+Piya-Boy@users.noreply.github.com>
2024-10-18 20:02:10 +0700

    Form Design

---
+++ src/components/forms/TeacherForm.tsx
@@ -0,0 +1,12 @@
+"use client";
+export default function TeacherForm({
+    type,
+    data,
+}:{
+    type: "create" | "update";
+    data: any;
+}) {
+  return (
+    <form className="">TeacherForm</form>
+  )
+}
