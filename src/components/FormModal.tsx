69ef0941956543e9152ef16c971092a4465d85a4
Piya Miang-Lae <87295280+Piya-Boy@users.noreply.github.com>
2024-10-19 16:38:03 +0700

    Update FormModal.tsx

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -3,6 +3,7 @@ import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined';
 import { useState } from "react";
 import dynamic from "next/dynamic";
 import Spinner from "./Spinner";
@@ -19,7 +20,7 @@ const forms: {
   [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
 } = {
   teacher: (type, data) => <TeacherForm type={type} data={data} />,
-  student: (type, data) => <StudentForm type={type} data={data} />
+  student: (type, data) => <StudentForm type={type} data={data} />,
 };
 export default function FormModal({
   table,
@@ -70,20 +71,37 @@ export default function FormModal({
 
   const Form = () => {
     return type === "delete" && id ? (
-      <form action="" className="p-4 flex flex-col gap-4">
-        <span className="text-center font-medium dark:text-gray-100">
+        <form action="" className="p-4 flex flex-col gap-4">
+             <div className="p-4 md:p-5 text-center">
+            <ErrorOutlineOutlinedIcon className="mx-auto mb-4 text-gray-400 w-16 h-16  dark:text-gray-200" />
+            <h3 className="mb-5 text-lg font-medium text-gray-500 dark:text-gray-100">
           All data will be lost. Are you sure you want to delete this {table}?
-        </span>
-        <button className="bg-red-700 hover:bg-red-600 text-white py-2 px-4 rounded-md border-none w-max self-center">
-          Delete
-        </button>
-      </form>
+            </h3>
+            <button
+              onClick={() => {
+                // Handle delete logic
+              }}
+              type="button"
+              className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+            >
+              Yes, I'm sure
+            </button>
+            <button
+              onClick={handleClose}
+              type="button"
+              className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            >
+              No, cancel
+            </button>
+       </div>
+        </form>
     ) : type === "create" || type === "update" ? (
       forms[table](type, data)
     ) : (
       "Form not found!"
     );
   };
+  
 
   return (
     <>
@@ -109,8 +127,8 @@ export default function FormModal({
               className="absolute top-4 right-4 cursor-pointer"
               onClick={handleClose}
             >
-              <CloseOutlinedIcon className="text-gray-400 dark:text-gray-500" />
-            </div>            
+              <CloseOutlinedIcon className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-5 h-5 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" />
+            </div>
           </div>
         </div>
       )}
