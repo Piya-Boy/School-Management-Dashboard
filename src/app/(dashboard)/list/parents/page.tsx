2d40ed78ac69a7c9446e98539cef7313d24d8637
Piya Miang-Lae <87295280+Piya-Boy@users.noreply.github.com>
2024-10-24 17:50:48 +0700

    Fetch Parents

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -5,15 +5,19 @@ import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import { parentsData, role } from "@/lib/data";
 import FormModal from "@/components/FormModal";
+import { Parent, Prisma, Student } from "@prisma/client";
+import { db } from "@/lib/db";
+import { ITEM_PER_PAGE } from "@/lib/settings";
 
-type Parent = {
-    id: number;
-    name: string;
-    email?: string;
-    students: string[];
-    phone: string;
-    address: string;
-  };
+// type Parent = {
+//     id: number;
+//     name: string;
+//     email?: string;
+//     students: string[];
+//     phone: string;
+//     address: string;
+//   };
+type ParentList = Parent & { students: Student[] };
 
 const columns = [
   {
@@ -40,34 +44,74 @@ const columns = [
     accessor: "action",
   },
 ];
-export default function ParentListPage() {
 
-  const renderRow = (item: Parent) => (
-    <tr
-      key={item.id}
-      className="border-b border-gray-200  dark:border-slate-600 even:bg-slate-50  dark:even:bg-slate-600 text-sm hover:bg-lamaPurpleLight dark:hover:bg-slate-600"
-    >
-      <td className="flex items-center gap-4 p-4">
-        <div className="flex flex-col">
-          <h3 className="font-semibold dark:text-gray-100">{item.name}</h3>
-          <p className="text-xs text-gray-500 dark:text-gray-400">{item?.email}</p>
-        </div>
-      </td>
-      <td className="hidden md:table-cell dark:text-gray-100">{item.students.join(",")}</td>
-      <td className="hidden md:table-cell dark:text-gray-100">{item.phone}</td>
-      <td className="hidden md:table-cell dark:text-gray-100">{item.address}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          {role === "admin" && (
-            <>
-              <FormModal table="parent" type="update" data={item} />
-              <FormModal table="parent" type="delete" id={item.id} />
-            </>
-          )}
-        </div>
-      </td>
-    </tr>
-  );
+
+const renderRow = (item: ParentList) => (
+  <tr
+    key={item.id}
+    className="border-b border-gray-200  dark:border-slate-600 even:bg-slate-50  dark:even:bg-slate-600 text-sm hover:bg-lamaPurpleLight dark:hover:bg-slate-600"
+  >
+    <td className="flex items-center gap-4 p-4">
+      <div className="flex flex-col">
+        <h3 className="font-semibold dark:text-gray-100">{item.name}</h3>
+        <p className="text-xs text-gray-500 dark:text-gray-400">{item?.email}</p>
+      </div>
+    </td>
+    <td className="hidden md:table-cell dark:text-gray-100">{item.students.map((student) => student.name).join(",")}</td>
+    <td className="hidden md:table-cell dark:text-gray-100">{item.phone}</td>
+    <td className="hidden md:table-cell dark:text-gray-100">{item.address}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        {role === "admin" && (
+          <>
+            <FormModal table="parent" type="update" data={item} />
+            <FormModal table="parent" type="delete" id={item.id} />
+          </>
+        )}
+      </div>
+    </td>
+  </tr>
+);
+export default async function ParentListPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+
+  
+  const { page, ...queryParams } = searchParams;
+
+  const p = page ? parseInt(page) : 1;
+
+  // URL PARAMS CONDITION
+
+  const query: Prisma.ParentWhereInput = {};
+
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "search":
+            query.name = { contains: value, mode: "insensitive" };
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  const [data, count] = await db.$transaction([
+    db.parent.findMany({
+      where: query,
+      include: {
+        students: true,
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+    }),
+    db.parent.count({ where: query }),
+  ]);
    
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0 dark:bg-slate-700">
@@ -92,9 +136,9 @@ export default function ParentListPage() {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={parentsData} />
+      <Table columns={columns} renderRow={renderRow} data={data} />
       {/* PAGINATION */}
-        <Pagination />
+        <Pagination page={p} count={count} />
     </div>
   );
 }
