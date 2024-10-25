9acc6784437f94b4d115e58d6aa156ac93c44e0d
Piya Miang-Lae <87295280+Piya-Boy@users.noreply.github.com>
2024-10-25 20:32:12 +0700

    Role Based Authentication

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -5,7 +5,10 @@ import EventCalendar from "@/components/EventCalendar";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
-export default function AdminPage() {
+export default function AdminPage({
+  searchParams,
+}: {
+  searchParams: { [keys: string]: string | undefined };}) {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
@@ -36,7 +39,7 @@ export default function AdminPage() {
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-      <EventCalendar/>
+      <EventCalendar searchParams={searchParams}/>
       <Announcements/>
       </div>
     </div>
